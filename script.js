d0d9fc90e84d79a8f6ae1c80ac805f33a8655f62
pkr86037 <pkr86037@gmail.com>
2024-12-20 13:50:03 +0530

    this is form

---
+++ script.js
@@ -0,0 +1,158 @@
+// // // let a = 15;
+// // // let b = 8;
+// // // console.log("A = 15 ", " &  B = 8", );
+// // // console.log("A + B =",a + b);
+// // // console.log("A - B =",a - b);
+// // // console.log("A * B =",a * b);
+// // // console.log("A / B =",a / b);
+// // // console.log("A ** B =",a ** b);
+// // // console.log("A % B =",a % b);
+// // // console.log("A ++ B =",a += b);
+// // // console.log("A -- B =",a -= b);
+
+// // // let a = 8;
+// // // let b = 3;
+// // // let c = (a - b);
+// // // console.log( "Answer is =",c * 2);
+
+// // // let a = 10;
+// // //  a /= 5;
+// // //  console.log(a);
+
+// // //  let x = 10;
+// // // x += 5;
+// // // x *= 2;
+// // // x /= 3;
+// // // console.log(x);
+
+// // // let saving = 0;
+// // // saving += 5;
+// // // saving += 5;
+// // // saving += 5;
+// // // saving += 5;
+
+// // // console.log(saving);
+
+// // // let x = 5
+// // // x++;
+// // // console.log("x =",x++) 
+
+// // // const a = 3;
+// // // const b = -2;
+
+// // // console.log(a > 0 && b > 0);
+
+
+// // // // Logical and &&
+
+// // // let score = 76;
+// // // let grade;
+
+// // // if (score >= 90 && score <= 100) {
+// // //     grade = "A";
+// // // } else if (score >= 80 && score <= 89) {
+// // //     grade = "B";
+// // // } else if (score >= 70 && score <= 79) {
+// // //     grade = "C";
+// // // }
+// // // console.log("Grade is =", grade);
+
+// // // let num = prompt("Enter a Number");
+// // // if (num % 5 === 0) {
+// // //     console.log(num, "= is multiple of five20");
+// // // } else {
+// // //     console.log(num, "= is not multiple of five")
+// // // }
+
+// // // Write a program to check if a number is even or odd using an if-else statement.
+
+// // // let num = 33;
+
+// // // if (num % 2 === 0 ){
+// // //     console.log(num,"= num is even");
+// // // } else{
+// // //     console.log(num,"= num is Odd");
+// // // }
+
+// // let num = 7;
+
+// // if(num > 0 ){
+// //     console.log("Enter Number is","positive");
+// // }else if (num < 0 ){
+// //     console.log("negetive");
+// // } else{
+// //     console.log("zero");
+// // }
+
+// // let age = 66;
+
+// // if (age < 13){
+// //     console.log("child");
+// // } else if (age >=13 && age < 20){
+// //     console.log("Teenager");
+// // } else if (age >= 20 && age <= 60){
+// //     console.log("Adult");
+// // }else{
+// //     console.log("senior Citizen");
+// // }
+// // let a = 12, b = 8, c = 15;
+// // if (a > b) {
+// //     if (a > c) {
+// //         console.log("Largest is: " + a);
+// //     } else {
+// //         console.log("Largest is: " + c);
+// //     }
+// // } else {
+// //     if (b > c) {
+// //         console.log("Largest is: " + b);
+// //     } else {
+// //         console.log("Largest is: " + c);
+// //     }
+// // }
+
+// // //Write a program to assign grades based on marks using a switch statement:
+
+// // let marks = 60;
+// // let grade;
+
+// // if (marks >= 90 && marks <= 100){
+// //     grade = " Grade A";
+// // }else if (marks >= 80 && marks <= 89){
+// //     grade = "Grade B";
+// // } else if (marks >= 70 && marks <= 79){
+// //     grade = "Grade C";
+// // } else if (marks >= 60 && marks <= 69){
+// //     grade = "Grade D";
+// // } else {
+// //     grade = "Fail";
+// // }
+// // console.log(grade);
+
+// // let Age =8;
+// // let leap;
+// // if( Age >= 35 && Age <= 40 ){
+// //    leap ="leap age";
+// // } else{
+// //    leap = "not leap age";
+// // }
+// // console.log(leap);
+
+// // let age = 2;
+
+// // if (age >= 18 ){
+// //     console.log("you can vote");
+// // } else{
+// //     console.log("you can not vote");
+// // }
+
+// let signal = "red";
+
+// if (signal === "red"){
+//     console.log("STOP");
+// } else if (signal === "yellow"){
+//     console.log("GET READY");
+// } else if (signal === "green"){
+//     console.log("GO");
+// }else{
+//     console.log("invilid");
+// }
